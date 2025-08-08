798a645519eee48fff15ba8f0b9215dfabffbc60
ichiragkumar <chiragroy3030@gmail.com>
2025-08-08 16:43:26 +0530

    inventry management added

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common';
 
 import { WalletModule } from './wallet/wallet.module';
 import { StoreModule } from './store/store.module';
+import { InventoryModule } from './inventory/inventory.module';
 
 @Module({
   imports: [
     WalletModule,
     StoreModule,
+    InventoryModule,
   ],
 })
 export class ApiModule {}
